110788311fbc35577992b94e2eac02b1d0621953
adityanarayan679 <adityanarayanbarik344@gmail.com>
2026-07-29 08:28:01 +0530

    Re-push all changes inc Hero

--- fix_about.ps1
+++
@@ -1,5 +0,0 @@
-$utf8NoBom = New-Object System.Text.UTF8Encoding($false)
-$path = 'D:\Projects\Portfolio\src\components\About.jsx'
-$content = [System.IO.File]::ReadAllText($path)
-$content = $content -replace "(?s)<div.*?FaGraduationCap.*?Driems Institute.*?</div>", ""
-[System.IO.File]::WriteAllText($path, $content, $utf8NoBom)
